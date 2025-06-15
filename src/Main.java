5a848a4a827f4d8ec374e4a744e62f7304ff37be
Ahmet Furkan Örek <33ahmetorek33@gmail.com>
2025-06-15 14:43:49 +0300

    this. ekledim

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        MayinTarlasi oyun = new MayinTarlasi(2, 2); // 5x5 boyutunda oyun
+        MayinTarlasi oyun = new MayinTarlasi(4, 4); // 5x5 boyutunda oyun
         oyun.oyunuBaslat();
     }
 }
